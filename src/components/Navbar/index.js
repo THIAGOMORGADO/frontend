da9eb4b6559c55678f380c192090cbed6848b47a
thiago morgado <thiago_morgado@hotmail.com>
2022-11-22 21:22:21 -0300

    ajuste nav bar mobile

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,18 +1,25 @@
-import React, {useState} from 'react'
-import {useNavigate} from 'react-router-dom';
-import { useAuth } from "../../context/AuthContext";
-import {Container, Button, Nav, Navbar, NavDropdown, Form } from 'react-bootstrap/';
-import  * as Icons from 'react-bootstrap-icons';
 
-import { FaRegPlusSquare } from "react-icons/fa";
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useAuth } from '../../context/AuthContext'
+import {
+  Container,
 
-import logo  from '../../assets/logo_white.png';
+  Nav,
+  Navbar,
+  NavDropdown,
+  
+} from 'react-bootstrap/'
+import * as Icons from 'react-bootstrap-icons'
+
+import { FaRegPlusSquare } from 'react-icons/fa'
+
+import logo from '../../assets/logo_white.png'
 
 export function NavBar() {
-  
-  const navigate = useNavigate();
-  const {currentUser, logout} = useAuth();
-  const [error, setError] = useState('');
+  const navigate = useNavigate()
+  const { currentUser, logout } = useAuth()
+  const [error, setError] = useState('')
 
   const handleHome = () => {
     navigate('/')
@@ -39,75 +46,94 @@ export function NavBar() {
   async function handleLogoutSubmit() {
     try {
       setError('')
-      await logout();
-    } catch(error) {
-      setError(`Falha no Logout`);
+      await logout()
+    } catch (error) {
+      setError(`Falha no Logout`)
     }
   }
 
   return (
     <>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" >
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container className="fntrbt">
-        <Navbar.Brand onClick={handleHome} className="linhaheader">
-         <img src={logo} alt="logo-renato-gonda"  width="200" onClick={handleHome} style={{cursor: 'pointer'}}/> 
-        </Navbar.Brand> 
-        {currentUser && 
-        <>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Buscar"
-              className="me-2 Input"
-              aria-label="Buscar..."              
+          <Navbar.Brand onClick={handleHome} className="linhaheader">
+            <img
+              src={logo}
+              alt="logo-renato-gonda"
+              width="100"
+              onClick={handleHome}
+              style={{ cursor: 'pointer' }}
             />
-            <Button variant="outline-success" onClick={handleBooks}><Icons.Search/></Button>
-          </Form>  
-        </>}
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto nav_bar_items">
+              {currentUser && (
+                <>
+                  <Nav.Link onClick={handleHome} className="navItem">
+                    Home
+                  </Nav.Link>
+                  <Nav.Link className="navItem itemmobile"> | </Nav.Link>
+                  <Nav.Link onClick={handleAbout} className="navItem">
+                    Renato Gonda
+                  </Nav.Link>
+                  <Nav.Link className="navItem itemmobile"> | </Nav.Link>
+                  <Nav.Link onClick={handleCorreraEgonda} className="navItem">
+                    Correra&Gonda
+                  </Nav.Link>
+                  <Nav.Link className="navItem itemmobile"> | </Nav.Link>
+                  <Nav.Link onClick={handleBooks} className="navItem">
+                    Livros
+                  </Nav.Link>
+                  <Nav.Link className="navItem itemmobile"> | </Nav.Link>
+                  <Nav.Link onClick={handleContacts} className="navItem">
+                    Contato
+                  </Nav.Link>
+                  <Nav.Link className="navItem itemmobile"> | </Nav.Link>
 
-          <Navbar.Toggle aria-controls="responsive-navbar-nav"  />
-          <Navbar.Collapse  id="responsive-navbar-nav" >
-            <Nav className="me-auto nav_bar_items" >
-            {currentUser && 
-              <>
-              <Nav.Link onClick={handleHome} className="navItem">Home</Nav.Link>  
-              <Nav.Link  className="navItem itemmobile"> | </Nav.Link>
-              <Nav.Link onClick={handleAbout} className="navItem">Renato Gonda</Nav.Link>
-              <Nav.Link  className="navItem itemmobile"> | </Nav.Link>
-              <Nav.Link onClick={handleCorreraEgonda} className="navItem">Correra&Gonda</Nav.Link>     
-              <Nav.Link  className="navItem itemmobile"> | </Nav.Link>
-              <Nav.Link onClick={handleBooks} className="navItem">Livros</Nav.Link>     
-              <Nav.Link  className="navItem itemmobile"> | </Nav.Link>
-              <Nav.Link onClick={handleContacts} className="navItem">Contato</Nav.Link>
-              <Nav.Link  className="navItem itemmobile"> | </Nav.Link>
-                      
-              {/* <Nav.Link onClick={handleAddBookPage}><strong><FaRegPlusSquare /> Livro</strong></Nav.Link> */}
-              {/* <Nav.Link  className="navItem itemmobile"> | </Nav.Link> */}
-              </>
-              }
-              <br/>             
-              <NavDropdown className="bg-drop" title="" id="navbarScrollingDropdown" >
-                {!currentUser ?
-                  <NavDropdown.Item onClick={handleLogin} bg="dark" className="Items">
+                  {/* <Nav.Link onClick={handleAddBookPage}><strong><FaRegPlusSquare /> Livro</strong></Nav.Link> */}
+                  {/* <Nav.Link  className="navItem itemmobile"> | </Nav.Link> */}
+                </>
+              )}
+              <br />
+              <NavDropdown
+                className="bg-drop"
+                title=""
+                id="navbarScrollingDropdown"
+              >
+                {!currentUser ? (
+                  <NavDropdown.Item
+                    onClick={handleLogin}
+                    bg="dark"
+                    className="Items"
+                  >
                     <Icons.PersonCircle className="icons" /> Login
                   </NavDropdown.Item>
-                :
-                <>
-                  <NavDropdown.Item onClick={() => handleLogoutSubmit()} bg="dark" className="Items">
-                    <Icons.PersonCircle className="icons" /> Logout
-                    <p>{error}</p>
-                  </NavDropdown.Item>
-                
-                  <NavDropdown.Item onClick={handleAddBookPage} bg="dark" className="Items">
-                    <FaRegPlusSquare /> Livro
-                  </NavDropdown.Item>
-                </>                  
-                }
+                ) : (
+                  <>
+                    <NavDropdown.Item
+                      onClick={() => handleLogoutSubmit()}
+                      bg="dark"
+                      className="Items"
+                    >
+                      <Icons.PersonCircle className="icons" /> Logout
+                      <p>{error}</p>
+                    </NavDropdown.Item>
+
+                    <NavDropdown.Item
+                      onClick={handleAddBookPage}
+                      bg="dark"
+                      className="Items"
+                    >
+                      <FaRegPlusSquare /> Livro
+                    </NavDropdown.Item>
+                  </>
+                )}
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        </Container>
+      </Navbar>
     </>
   )
 }
